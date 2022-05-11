1157389336495acfbbd40a0c39c318a2313a3b4c
Andy Bell <andyjsbell@gmail.com>
2022-05-11 12:34:41 +0000

    update to new format

--- demo_connect_four.sh
+++ demo_connect_four.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 # setup:
-# build ajuna node with skip-ias-check
-#   cargo build --release --features skip-ias-check
+# build ajuna node with skip-ias-check on branch "validateer-setup"
+#   cargo build --release --features solo,skip-ias-check
 #
 # run ajuna node
-#   target/release/ajuna  --dev --tmp --ws-port <NODEPORT> --port 30385 --rpc-port <RPCPORT>
+#   ./target/release/ajuna-solo  --dev --tmp --ws-port <NODEPORT>
 #
-# run worker
+# run worker inside the bin folder:
 #   rm light_client_db.bin
 #   rm -r shards
 #   rm -r sidechain_db
 #   export RUST_LOG=integritee_service=info,ita_stf=debug
-#   integritee-service init_shard
-#   integritee-service shielding-key
-#   integritee-service signing-key
-#   integritee-service -P <WORKERPORT> -p <NODEPORT> -r 3485 run --dev --skip-ra
+#   ./integritee-service init-shard
+#   ./integritee-service shielding-key
+#   ./integritee-service signing-key
+#   ./integritee-service -P <WORKERPORT> -p <NODEPORT> -r <REMOTE-ATTESTATION-PORT> run --dev --skip-ra
 #
 # then run this script
 
 # usage:
 #  export RUST_LOG=integritee-cli=info,ita_stf=info
-#  demo_connect_four.sh -p <NODEPORT> -P <WORKERPORT> -m <FILENAME>
+#  ./demo_connect_four.sh -p <NODEPORT> -P <WORKERPORT> -m file
 #
-# if -m <FILENAME> is set, the mrenclave will be read from file <FILENAME>
+# if -m file is set, the mrenclave will be read from file  ~/mrenclave.b58
+
 
 while getopts ":m:p:P:" opt; do
     case $opt in
@@ -73,12 +74,12 @@ echo "  Bob's account = ${ACCOUNTBOB}"
 echo ""
 
 echo "* Issue ${BALANCE} tokens to Alice's account"
-${CLIENT} trusted set-balance ${ACCOUNTALICE} ${BALANCE} --mrenclave=${MRENCLAVE} --direct
+${CLIENT} trusted --mrenclave ${MRENCLAVE} --direct set-balance ${ACCOUNTALICE} ${BALANCE}
 echo ""
 sleep 1
 
 echo "* Issue ${BALANCE} tokens to Bob's account"
-${CLIENT} trusted set-balance ${ACCOUNTBOB} ${BALANCE} --mrenclave=${MRENCLAVE} --direct
+${CLIENT} trusted --mrenclave ${MRENCLAVE} --direct set-balance ${ACCOUNTBOB} ${BALANCE}
 echo ""
 sleep 1
 
@@ -96,22 +97,22 @@ echo "waiting"
 sleep 45
 
 echo "Turn for Alice (Player 1)"
-${CLIENT} trusted play-turn ${ACCOUNTALICE} 3 --direct --mrenclave=${MRENCLAVE}
+${CLIENT} trusted --mrenclave ${MRENCLAVE} --direct play-turn ${ACCOUNTALICE} 3
 echo ""
 sleep 1
 
 echo "Turn for Bob (Player 2)"
-${CLIENT} trusted play-turn ${ACCOUNTBOB} 4 --direct --mrenclave=${MRENCLAVE}
+${CLIENT} trusted --mrenclave ${MRENCLAVE} --direct play-turn ${ACCOUNTBOB} 4
 echo ""
 sleep 1
 
 echo "Turn for Alice (Player 1)"
-${CLIENT} trusted play-turn ${ACCOUNTALICE} 2 --direct --mrenclave=${MRENCLAVE}
+${CLIENT} trusted --mrenclave ${MRENCLAVE} --direct play-turn ${ACCOUNTALICE} 2
 echo ""
 sleep 1
 
 echo "Turn for Bob (Player 2)"
-${CLIENT} trusted play-turn ${ACCOUNTBOB} 3 --direct --mrenclave=${MRENCLAVE}
+${CLIENT} trusted --mrenclave ${MRENCLAVE} --direct play-turn ${ACCOUNTBOB} 3
 echo ""
 sleep 1
 
@@ -119,28 +120,27 @@ echo "waiting"
 sleep 5
 
 echo "Board after 2 turns"
-${CLIENT} trusted get-board ${ACCOUNTBOB} --direct --mrenclave=${MRENCLAVE}
+${CLIENT} trusted --mrenclave ${MRENCLAVE} get-board ${ACCOUNTBOB}
 echo ""
 sleep 1
 
 
 echo "Turn for Alice (Player 1)"
-${CLIENT} trusted play-turn ${ACCOUNTALICE} 2 --direct --mrenclave=${MRENCLAVE}
+${CLIENT} trusted --mrenclave ${MRENCLAVE} --direct play-turn ${ACCOUNTALICE} 2
 echo ""
 sleep 1
 
 echo "Turn for Bob (Player 2)"
-${CLIENT} trusted play-turn ${ACCOUNTBOB} 5 --direct --mrenclave=${MRENCLAVE}
-echo ""
+${CLIENT} trusted  --mrenclave ${MRENCLAVE} --direct play-turn ${ACCOUNTBOB} 5
 sleep 1
 
 echo "Turn for Alice (Player 1)"
-${CLIENT} trusted play-turn ${ACCOUNTALICE} 2 --direct --mrenclave=${MRENCLAVE}
+${CLIENT} trusted --mrenclave  ${MRENCLAVE}  --direct  play-turn ${ACCOUNTALICE} 2
 echo ""
 sleep 1
 
 echo "Turn for Bob (Player 2)"
-${CLIENT} trusted play-turn ${ACCOUNTBOB} 1 --direct --mrenclave=${MRENCLAVE}
+${CLIENT} trusted --mrenclave ${MRENCLAVE} --direct  play-turn ${ACCOUNTBOB} 1
 echo ""
 sleep 1
 
@@ -148,12 +148,12 @@ echo "waiting"
 sleep 5
 
 echo "Board after 4 turns"
-${CLIENT} trusted get-board ${ACCOUNTBOB} --direct --mrenclave=${MRENCLAVE}
+${CLIENT} trusted --mrenclave ${MRENCLAVE} get-board ${ACCOUNTBOB}
 echo ""
 sleep 1
 
 echo "Turn for Alice  (Player 1)"
-${CLIENT} trusted play-turn ${ACCOUNTALICE} 2 --direct --mrenclave=${MRENCLAVE}
+${CLIENT} trusted --mrenclave  ${MRENCLAVE} --direct  play-turn ${ACCOUNTALICE} 2
 echo ""
 sleep 1
 
@@ -161,8 +161,6 @@ echo "waiting"
 sleep 5
 
 echo "Board after end of game"
-${CLIENT} trusted get-board ${ACCOUNTBOB} --direct --mrenclave=${MRENCLAVE}
+${CLIENT} trusted --mrenclave ${MRENCLAVE} get-board ${ACCOUNTBOB}
 echo ""
-
-
 
